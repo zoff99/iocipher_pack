e9cbdcdef97030214429ef9d98bd1fb843765bd6
zoff99 <zoff99@users.noreply.github.com>
2024-10-02 16:07:07 +0200

    build types

--- compile_docker_all.sh
+++ compile_docker_all.sh
@@ -7,83 +7,74 @@ export _HOME_
 
 logfile="$_HOME_""/compile.log"
 
-build_only_aar=0
+build_openssl=1
+build_sqlfs=1
 if [ "$1""x" == "aarx" ]; then
-    build_only_aar=1
+    echo "====== build type: only AAR ======"
+    build_openssl=0
+    build_sqlfs=0
+fi
+if [ "$1""x" == "sqlfsx" ]; then
+    echo "====== build type: sqlfs and AAR ======"
+    build_openssl=0
+    build_sqlfs=1
+fi
+if [ "$build_openssl""x" == "1x" ] && [ "$build_sqlfs""x" == "1x" ]; then
+    echo "====== build type: full ======"
 fi
 
 cd "$_HOME_""/"
 rm -f "$logfile"
 
+if [ "$build_openssl""x" == "1x" ]; then
+    cd ./001_src_openssl/.localrun/ || exit 1
+    echo "build/upate docker container"
+    ./docker_linux_fast.sh build >> "$logfile" 2>&1 || exit 1
+    echo "build openssl for android and linux"
+    ./docker_linux_fast.sh >> "$logfile" 2>&1 || exit 1
+fi
 
-if [ "$build_only_aar""x" == "0x" ]; then
-###################################
-###################################
-###################################
+if [ "$build_sqlfs""x" == "1x" ]; then
+    cd "$_HOME_""/"
+    ## maybe clean, in case a headerfile gets removed in th future? ## rm -Rf ./002_src_libsqlfs/openssl_includes/ >> "$logfile" 2>&1
+    mkdir -p ./002_src_libsqlfs/openssl_includes/ >> "$logfile" 2>&1
+    echo "install openssl includes"
+    cp -av ./001_src_openssl/.localrun/debian_12_linux/artefacts/linux_debian12_x86_64/include/* ./002_src_libsqlfs/openssl_includes/ >> "$logfile" 2>&1 || exit 1
+    echo "install openssl linux libs"
+    cp -av ./001_src_openssl/.localrun/debian_12_linux/artefacts/linux_debian12_x86_64/lib*.a ./002_src_libsqlfs/openssl_libs/ >> "$logfile" 2>&1 || exit 1
+    echo "install openssl android libs"
+    cp -av ./001_src_openssl/.localrun/debian_12_linux/artefacts/android-arm/lib*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/armeabi-v7a/ >> "$logfile" 2>&1 || exit 1
+    cp -av ./001_src_openssl/.localrun/debian_12_linux/artefacts/android-arm64/lib*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/arm64-v8a/ >> "$logfile" 2>&1 || exit 1
+    cp -av ./001_src_openssl/.localrun/debian_12_linux/artefacts/android-x86/lib*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86/ >> "$logfile" 2>&1 || exit 1
+    cp -av ./001_src_openssl/.localrun/debian_12_linux/artefacts/android-x86_64/lib*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86_64/ >> "$logfile" 2>&1 || exit 1
 
-cd ./001_src_openssl/.localrun/ || exit 1
-echo "build/upate docker container"
-./docker_linux_fast.sh build >> "$logfile" 2>&1 || exit 1
-echo "build openssl for android and linux"
-./docker_linux_fast.sh >> "$logfile" 2>&1 || exit 1
-
-
-cd "$_HOME_""/"
-## maybe clean, in case a headerfile gets removed in th future? ## rm -Rf ./002_src_libsqlfs/openssl_includes/ >> "$logfile" 2>&1
-mkdir -p ./002_src_libsqlfs/openssl_includes/ >> "$logfile" 2>&1
-echo "install openssl includes"
-cp -av ./001_src_openssl/.localrun/debian_12_linux/artefacts/linux_debian12_x86_64/include/* ./002_src_libsqlfs/openssl_includes/ >> "$logfile" 2>&1 || exit 1
-
-echo "install openssl linux libs"
-cp -av ./001_src_openssl/.localrun/debian_12_linux/artefacts/linux_debian12_x86_64/lib*.a ./002_src_libsqlfs/openssl_libs/ >> "$logfile" 2>&1 || exit 1
-
-echo "install openssl android libs"
-cp -av ./001_src_openssl/.localrun/debian_12_linux/artefacts/android-arm/lib*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/armeabi-v7a/ >> "$logfile" 2>&1 || exit 1
-cp -av ./001_src_openssl/.localrun/debian_12_linux/artefacts/android-arm64/lib*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/arm64-v8a/ >> "$logfile" 2>&1 || exit 1
-cp -av ./001_src_openssl/.localrun/debian_12_linux/artefacts/android-x86/lib*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86/ >> "$logfile" 2>&1 || exit 1
-cp -av ./001_src_openssl/.localrun/debian_12_linux/artefacts/android-x86_64/lib*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86_64/ >> "$logfile" 2>&1 || exit 1
-
-
-cd "$_HOME_""/"
-cd ./002_src_libsqlfs/ || exit 1
-
-echo "build sqlfs and sqlcipher for linux"
-make -j2 >> "$logfile" 2>&1 || exit 1
-
-cd "$_HOME_""/"
-cd ./002_src_libsqlfs/.localrun/ || exit 1
-
-echo "build/upate docker container"
-./docker_linux_fast.sh build >> "$logfile" 2>&1 || exit 1
-
-echo "build sqlfs and sqlcipher for android"
-./docker_linux_fast.sh >> "$logfile" 2>&1 || exit 1
-
-
-echo "install sqlfs and sqlcipher android libs"
-cd "$_HOME_""/"
-cp -av ./002_src_libsqlfs/.localrun/debian_12_linux/artefacts/android-arm/*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/armeabi-v7a/ >> "$logfile" 2>&1 || exit 1
-cp -av ./002_src_libsqlfs/.localrun/debian_12_linux/artefacts/android-arm64/*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/arm64-v8a/ >> "$logfile" 2>&1 || exit 1
-cp -av ./002_src_libsqlfs/.localrun/debian_12_linux/artefacts/android-x86/*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86/ >> "$logfile" 2>&1 || exit 1
-cp -av ./002_src_libsqlfs/.localrun/debian_12_linux/artefacts/android-x86_64/*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86_64/ >> "$logfile" 2>&1 || exit 1
-
-echo "install sqlfs android includes"
-cd "$_HOME_""/"
-cp -av ./002_src_libsqlfs/sqlfs.h ./003_src_iocipher/libiocipher2-c/src/main/cpp/libsqlfs/ >> "$logfile" 2>&1 || exit 1
-cp -av ./002_src_libsqlfs/sqlfs_internal.h ./003_src_iocipher/libiocipher2-c/src/main/cpp/libsqlfs/ >> "$logfile" 2>&1 || exit 1
-
-###################################
-###################################
-###################################
+    cd "$_HOME_""/"
+    cd ./002_src_libsqlfs/ || exit 1
+    echo "build sqlfs and sqlcipher for linux"
+    make -j2 >> "$logfile" 2>&1 || exit 1
+    cd "$_HOME_""/"
+    cd ./002_src_libsqlfs/.localrun/ || exit 1
+    echo "build/upate docker container"
+    ./docker_linux_fast.sh build >> "$logfile" 2>&1 || exit 1
+    echo "build sqlfs and sqlcipher for android"
+    ./docker_linux_fast.sh >> "$logfile" 2>&1 || exit 1
+    echo "install sqlfs and sqlcipher android libs"
+    cd "$_HOME_""/"
+    cp -av ./002_src_libsqlfs/.localrun/debian_12_linux/artefacts/android-arm/*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/armeabi-v7a/ >> "$logfile" 2>&1 || exit 1
+    cp -av ./002_src_libsqlfs/.localrun/debian_12_linux/artefacts/android-arm64/*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/arm64-v8a/ >> "$logfile" 2>&1 || exit 1
+    cp -av ./002_src_libsqlfs/.localrun/debian_12_linux/artefacts/android-x86/*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86/ >> "$logfile" 2>&1 || exit 1
+    cp -av ./002_src_libsqlfs/.localrun/debian_12_linux/artefacts/android-x86_64/*.a ./003_src_iocipher/libiocipher2-c/src/main/jniLibs/x86_64/ >> "$logfile" 2>&1 || exit 1
+    echo "install sqlfs android includes"
+    cd "$_HOME_""/"
+    cp -av ./002_src_libsqlfs/sqlfs.h ./003_src_iocipher/libiocipher2-c/src/main/cpp/libsqlfs/ >> "$logfile" 2>&1 || exit 1
+    cp -av ./002_src_libsqlfs/sqlfs_internal.h ./003_src_iocipher/libiocipher2-c/src/main/cpp/libsqlfs/ >> "$logfile" 2>&1 || exit 1
 fi
 
 cd "$_HOME_""/"
 cd ./003_src_iocipher/ || exit 1
-
 echo "build iocipher for android"
 ./gradlew build >> "$logfile" 2>&1 || exit 1
 ./gradlew :libiocipher2-c:assemble >> "$logfile" 2>&1 || exit 1
-
 cd "$_HOME_""/"
 echo "AAR artefacts"
 ls -1 ./003_src_iocipher/libiocipher2-c/build/outputs/aar/libiocipher2-c-debug.aar || echo "NO ERR" 2>/dev/null
