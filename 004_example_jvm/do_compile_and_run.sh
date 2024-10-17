541b5807d2da4941fd92c03af15501b3f099b425
zoff99 <zoff99@users.noreply.github.com>
2024-10-17 09:12:00 +0200

    compile and run script tweaks

--- 004_example_jvm/do_compile_and_run.sh
+++ 004_example_jvm/do_compile_and_run.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
-#javac -classpath ".:iocipher_linux-1.0.3.jar" FileList.java FileRenderer.java TextFileFilter.java || exit 1
-#export ASAN_OPTIONS="halt_on_error=true:detect_leaks=0:handle_segv=0"
-#LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libasan.so.6.0.0 \
-#java -classpath ".:iocipher_linux-1.0.3.jar" -Dsun.java2d.uiScale=2.5 FileList
+# copy the current JNI linux jar to this subdir
+if [ -e ../003_src_iocipher/linux_jni/c_src/iocipher_linux-1.0.3.jar ]; then
+  cp -v ../003_src_iocipher/linux_jni/c_src/iocipher_linux-1.0.3.jar ./iocipher_linux-1.0.3.jar
+fi
 
+# ASAN run
+# javac -classpath ".:iocipher_linux-1.0.3.jar" FileManager.java FileDrop.java && \
+# (export ASAN_OPTIONS="halt_on_error=true:detect_leaks=0:handle_segv=0" ; \
+# LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libasan.so.6.0.0 java -classpath ".:iocipher_linux-1.0.3.jar" FileManager "$@" )
 
+# regular run
 javac -classpath ".:iocipher_linux-1.0.3.jar" FileManager.java FileDrop.java && \
-(export ASAN_OPTIONS="halt_on_error=true:detect_leaks=0:handle_segv=0" ; \
-LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libasan.so.6.0.0 java -classpath ".:iocipher_linux-1.0.3.jar" FileManager "$@" )
+java -classpath ".:iocipher_linux-1.0.3.jar" FileManager "$@"
