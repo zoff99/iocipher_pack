16f486a0c4782e4746d483630b5802f94a8a335d
zoff99 <zoff99@users.noreply.github.com>
2025-01-26 15:24:14 +0100

    new version 1.0.7

--- 006_example_python/sqlfs.h
+++ 006_example_python/sqlfs.h
@@ -32,7 +32,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 extern "C" {
 #endif
 
-#define SQLFS_VERSION   "2.0.1"
+#define SQLFS_VERSION   "2.0.2"
 
 
 #ifdef HAVE_LIBFUSE
