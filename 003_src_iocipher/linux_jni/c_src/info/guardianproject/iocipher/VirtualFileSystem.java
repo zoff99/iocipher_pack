906808ecb60b5ef32bd01346f124998bafdac497
zoff99 <zoff99@users.noreply.github.com>
2025-12-12 15:18:54 +0100

    new version 1.0.20

--- 003_src_iocipher/linux_jni/c_src/info/guardianproject/iocipher/VirtualFileSystem.java
+++ 003_src_iocipher/linux_jni/c_src/info/guardianproject/iocipher/VirtualFileSystem.java
@@ -14,7 +14,7 @@ import javax.crypto.SecretKey;
  */
 public class VirtualFileSystem {
 
-    final public static String IOCIPHER_JNI_VERSION = "1.0.19";
+    final public static String IOCIPHER_JNI_VERSION = "1.0.20";
 
     /**
      * Empty dbFile results in an in memory database
