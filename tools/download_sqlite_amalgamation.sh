839ad91bf5c48d0d01ec935f135f44f03ecee339
zoff99 <zoff99@users.noreply.github.com>
2026-03-11 20:11:16 +0100

    use current year in url

--- tools/download_sqlite_amalgamation.sh
+++ tools/download_sqlite_amalgamation.sh
@@ -2,7 +2,8 @@
 
 _SQLITE_VERSION_="3520000"
 
-amalgamation_url="https://sqlite.org/2025/sqlite-amalgamation-""$_SQLITE_VERSION_"".zip"
+current_year=$(date +%Y 2>/dev/null)
+amalgamation_url="https://sqlite.org/""$current_year""/sqlite-amalgamation-""$_SQLITE_VERSION_"".zip"
 
 
 _HOME2_=$(dirname $0)
@@ -12,13 +13,13 @@ export _HOME_
 
 basedir="$_HOME_""/../"
 
-cd "$basedir""/002_src_libsqlfs/"
+cd "$basedir""/002_src_libsqlfs/" || exit 1
 rm -f amalgamation.zip
 
-wget "$amalgamation_url" -O amalgamation.zip
+wget "$amalgamation_url" -O amalgamation.zip || exit 1
 mkdir "$basedir""/002_src_libsqlfs/sqlite/"
-cd "$basedir""/002_src_libsqlfs/sqlite/"
-unzip -j -o "$basedir""/002_src_libsqlfs/"amalgamation.zip
+cd "$basedir""/002_src_libsqlfs/sqlite/" || exit 1
+unzip -j -o "$basedir""/002_src_libsqlfs/"amalgamation.zip || exit 1
 cd "$basedir"
 
 cd "$basedir""/002_src_libsqlfs/"
