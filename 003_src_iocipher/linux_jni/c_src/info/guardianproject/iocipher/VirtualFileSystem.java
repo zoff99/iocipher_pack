d46fb2b5aee3fa79637b7e6fa9cc37b560148b65
zoff99 <zoff99@users.noreply.github.com>
2026-03-24 09:51:05 +0100

    new version 1.0.24

--- 003_src_iocipher/linux_jni/c_src/info/guardianproject/iocipher/VirtualFileSystem.java
+++ 003_src_iocipher/linux_jni/c_src/info/guardianproject/iocipher/VirtualFileSystem.java
@@ -14,7 +14,7 @@ import javax.crypto.SecretKey;
  */
 public class VirtualFileSystem {
 
-    final public static String IOCIPHER_JNI_VERSION = "1.0.23";
+    final public static String IOCIPHER_JNI_VERSION = "1.0.24";
 
     /**
      * Empty dbFile results in an in memory database
--- README.md
+++ README.md
@@ -64,7 +64,7 @@ allprojects {
 
 add to your module `build.gradle`<br>
 ```
-implementation 'com.github.zoff99:pkgs_guardianprojectIOCipher:1.0.23'
+implementation 'com.github.zoff99:pkgs_guardianprojectIOCipher:1.0.24'
 ```
 
 then see
