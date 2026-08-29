5ab38527e3c345699b6469845d2318fbafbec69e
zoff99 <zoff99@users.noreply.github.com>
2026-08-29 18:56:31 +0200

    rebuild all

--- 003_src_iocipher/libiocipher2-c/src/main/java/info/guardianproject/iocipher/VirtualFileSystem.java
+++ 003_src_iocipher/libiocipher2-c/src/main/java/info/guardianproject/iocipher/VirtualFileSystem.java
@@ -14,7 +14,7 @@ import javax.crypto.SecretKey;
  */
 public class VirtualFileSystem {
 
-    final public static String IOCIPHER_JNI_VERSION = "1.0.31";
+    final public static String IOCIPHER_JNI_VERSION = "1.0.32";
 
     /**
      * Empty dbFile results in an in memory database
